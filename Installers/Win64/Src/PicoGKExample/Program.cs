a30f08928225b23d9d8d575028a4722e4cebb6f7
Lin Kayser <github@lsk.me>
2023-11-24 18:30:09 +0400

    Shuffled folders, reset Mac installer and added submodules

--- Installers/Win64/Src/PicoGKExample/Program.cs
+++
@@ -1,3 +0,0 @@
-﻿using PicoGK;
-
-Library.Go(0.5f, PicoGKExamples.BooleanShowCase.Task);
---
+++ PicoGKExample/Program.cs
@@ -0,0 +1,11 @@
+﻿
+try
+{
+	PicoGK.Library.Go(0.5f, PicoGKExamples.BooleanShowCase.Task);
+}
+
+catch (Exception e)
+{
+	// Apparently something went wrong, output here
+	Console.WriteLine(e);
+}
